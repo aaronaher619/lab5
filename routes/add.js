7718a1579d5e2118d34940f287aef88175612ca0
Aaron Hernandez <aah007@ucsd.edu>
2021-02-04 10:17:08 -0800

    Done

--- routes/add.js
+++ routes/add.js
@@ -3,7 +3,6 @@ var data = require("../data.json");
 exports.addFriend = function(request, response) {
 	var friendName = request.query.name;
 	var friendDescription = request.query.description;
-	//console.log(friendName + friendDescription);
 
 	var newFriend = {
 		"name": friendName,
@@ -12,6 +11,5 @@ exports.addFriend = function(request, response) {
 	};
 
 	data.friends.push(newFriend);
-	console.log(data);
-	//response.render('add', data);
+	response.render('index', data);
 }
